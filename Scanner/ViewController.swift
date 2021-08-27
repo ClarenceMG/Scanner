33f1b1076d76b79dc37d8de83e204d1643626170
ClarenceMG <clarence.montenegro@gmail.com>
2021-08-27 15:42:29 -0600

    Initial Commit

---
+++ Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by Clarence Montenegro on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
